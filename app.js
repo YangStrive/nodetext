d93243f24cfb043cfcae9e6c19e03f605c43688c
yangfan1 <yangfan1@doumi.com>
2019-03-02 15:18:25 +0800

    no message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World\n 通过node 搭建的web服务器');
 });
 
 server.listen(port, hostname, () => {
